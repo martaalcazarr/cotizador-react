c98999f18c36b7751cfbe81bfd70e5505cb4b975
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-18 11:24:32 -0300

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {useState}  from 'react'
 import Header from "./components/Header"
+import Button from './components/Button';
 
 function App() {
   const [cantidad, setCantidad] = useState(2500000)
@@ -11,9 +12,36 @@ function App() {
   function handleChange(e){
     setCantidad(+e.target.value)
   }
+
+  function handleClickDecremento(e){
+    const valor = cantidad - STEP
+    if(valor < MIN){
+      alert('Cantidad no válida')
+      return
+    }
+    setCantidad(valor)
+  }
+  function handleClickIncremento(e){
+    const valor = cantidad + STEP
+    if(valor > MAX){
+      alert('Cantidad no válida')
+      return
+    }
+    setCantidad(valor)
+  }
   return (
    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div className="flex justify-between my-6">
+       <Button
+        operador='-'
+        fn={handleClickDecremento}
+       />
+       <Button
+        operador='+'
+        fn={handleClickIncremento} />
+    </div>
     <input 
       type='range' 
       className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
